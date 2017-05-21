595e0a7cec7d51d26ef793ce6ae408462301cca4
ssnover95 <sws7379@rit.edu>
2017-05-20 22:45:43 -0400

    Initial files for Arduino library quick implementation.

---
+++ timer.h
@@ -0,0 +1,18 @@
+/*
+ * File: timer.h
+ * Author: Shane Snover
+ */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+/* ============================================================================
+ * Public Function Declarations
+ * ============================================================================
+*/
+
+/* Initializes and sets the overflow value for the timer driving the
+ * applications' run frequency. */
+void timer_init(void);
+
+#endif /* TIMER_H */
